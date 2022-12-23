c3dd355b60267e7a1bda7f8737d9e39a5e647881
H3c7o4 <hectorvladitok@gmail.com>
2022-12-23 15:27:42 +0100

    Adding a new file

--- factorize.c
+++ factorize.c
@@ -4,7 +4,7 @@
  * factorize - The function factorize a number
  * @buffer: pointer to the address of the number
  *
- * Author: Thaoban Abdrasheed
+ * Author: ITOK Steve Hector
  * Return: int
  */
 int factorize(char *buffer)
